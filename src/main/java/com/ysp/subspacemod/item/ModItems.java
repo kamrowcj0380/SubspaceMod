42235c0d9980e75723738860647566e5cbca4f76
Andrew Linzmeier <300050292@fvtc.edu>
2023-10-03 12:03:47 -0500

    makes painful mac and cheese into a food item

--- src/main/java/com/ysp/subspacemod/item/ModItems.java
+++ src/main/java/com/ysp/subspacemod/item/ModItems.java
@@ -5,6 +5,9 @@ import com.ysp.subspacemod.entity.ModEntityTypes;
 import com.ysp.subspacemod.item.custom.ApplePipeItem;
 import com.ysp.subspacemod.item.custom.NancyItem;
 import com.ysp.subspacemod.item.custom.ShawnPhoneItem;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.common.ForgeSpawnEggItem;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -32,15 +35,17 @@ public class ModItems {
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SUBSPACE_TAB)));
     public static final RegistryObject<Item> QUAKE_PRO_GLASSES = ITEMS.register("quake_pro_glasses",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SUBSPACE_TAB).stacksTo(1)));
+    //Adds nancy item with max stack of 1 and 64 durability
     public static final RegistryObject<Item> NANCY = ITEMS.register("nancy",
             () -> new NancyItem(new Item.Properties().tab(ModCreativeModeTab.SUBSPACE_TAB).stacksTo(1).durability(64)));
     public static final RegistryObject<Item> SHAWN_PHONE = ITEMS.register("shawn_phone",
             () -> new ShawnPhoneItem(new Item.Properties().tab(ModCreativeModeTab.SUBSPACE_TAB).stacksTo(1)));
+    //painful mac and cheese food item that deals instant damage 1 when eaten
     public static final RegistryObject<Item> PAINFUL_MAC_AND_CHEESE = ITEMS.register("painful_mac_and_cheese",
-            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SUBSPACE_TAB).stacksTo(1)));
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.SUBSPACE_TAB).food(new FoodProperties.Builder().nutrition(2).saturationMod(0.3F).effect(new MobEffectInstance(MobEffects.HARM, 1, 0), 1F).build())));
 
 
-    //Adds an apple pipe item with a max stack of 1
+    //Adds an apple pipe item with a max stack of 1 and 10 durability
     public static final RegistryObject<Item> APPLE_PIPE = ITEMS.register("apple_pipe",
             () -> new ApplePipeItem(new Item.Properties().tab(ModCreativeModeTab.SUBSPACE_TAB).stacksTo(1).durability(10)));
 
