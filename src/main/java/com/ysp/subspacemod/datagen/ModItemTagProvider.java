90c0508a299f495675bc8a80c99d4bad6cbe2af4
Andrew Linzmeier <300050292@fvtc.edu>
2023-10-06 11:36:51 -0500

    changed curios api version to 5.1.1.0 due to crashes, updated tag data generator to assign items to curios tags

--- src/main/java/com/ysp/subspacemod/datagen/ModItemTagProvider.java
+++ src/main/java/com/ysp/subspacemod/datagen/ModItemTagProvider.java
@@ -3,20 +3,42 @@ package com.ysp.subspacemod.datagen;
 import com.ysp.subspacemod.SubspaceMod;
 import com.ysp.subspacemod.block.ModBlocks;
 import com.ysp.subspacemod.item.ModItems;
+import net.minecraft.core.Registry;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import top.theillusivec4.curios.api.CuriosApi;
 
 public class ModItemTagProvider extends ItemTagsProvider {
+
+    private static final TagKey<Item> BELT = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(CuriosApi.MODID, "belt"));
+    private static final TagKey<Item> HEAD = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(CuriosApi.MODID, "head"));
+    private static final TagKey<Item> HANDS = TagKey.create(Registry.ITEM_REGISTRY, new ResourceLocation(CuriosApi.MODID, "hands"));
     public ModItemTagProvider(DataGenerator generator, BlockTagsProvider blockTags, ExistingFileHelper helper) {
         super(generator, blockTags, SubspaceMod.MOD_ID, helper);
     }
     @Override
     protected void addTags() {
         tag(Tags.Items.ORES)
-                .add(ModItems.RAW_SUBSPACE.get());
+                .add(ModItems.RAW_SUBSPACE.get()
+        );
+
+        tag(BELT).add(
+                ModItems.NANCY.get()
+        );
+
+        tag(HEAD).add(
+                ModItems.QUAKE_PRO_GLASSES.get()
+        );
+
+        tag(HANDS).add(
+                ModItems.OGRE_HANDS.get()
+        );
     }
 
     @Override
