ade0819b4b56a7197bda73d63b9d2d1044b73528
Andrew Linzmeier <300050292@fvtc.edu>
2023-10-19 10:30:50 -0500

    updaded shawn entity to be neutral until attacked

--- src/main/java/com/ysp/subspacemod/entity/custom/ShawnEntity.java
+++ src/main/java/com/ysp/subspacemod/entity/custom/ShawnEntity.java
@@ -1,21 +1,32 @@
 package com.ysp.subspacemod.entity.custom;
 
+import net.minecraft.util.TimeUtil;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.NeutralMob;
 import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
-import net.minecraft.world.entity.ai.goal.ZombieAttackGoal;
 import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
+import net.minecraft.world.entity.ai.goal.target.ResetUniversalAngerTargetGoal;
 import net.minecraft.world.entity.monster.Monster;
-import net.minecraft.world.entity.monster.ZombifiedPiglin;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
 
-public class ShawnEntity extends Monster {
+import java.util.UUID;
+
+public class ShawnEntity extends Monster implements NeutralMob {
     public ShawnEntity(EntityType<? extends Monster> entityType, Level level) {
         super(entityType, level);
     }
+
+    private static final UniformInt PERSISTENT_ANGER_TIME = TimeUtil.rangeOfSeconds(20, 39);
+    private int remainingPersistentAngerTime;
+    @javax.annotation.Nullable
+    private UUID persistentAngerTarget;
 
     public static AttributeSupplier setAttributes(){
         //Sets the monster entities attributes
@@ -31,7 +42,51 @@ public class ShawnEntity extends Monster {
     //Adds a behavior goal ai to attack the nearest player
     @Override
     protected void registerGoals(){
-        this.goalSelector.addGoal(0, new MeleeAttackGoal(this, 1.0D, false));
-        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, true));
+        //allows mob to attack
+        this.goalSelector.addGoal(4, new MeleeAttackGoal(this, 1.0D, false));
+        //allows to find nearest target
+        this.targetSelector.addGoal(1, new NearestAttackableTargetGoal<>(this, Player.class, 10, true, false, this::isAngryAt));
+        //allows to be angry at something that hurts it
+        this.targetSelector.addGoal(2, new HurtByTargetGoal(this));
+        //anger wears off
+        this.targetSelector.addGoal(3, new ResetUniversalAngerTargetGoal<>(this, true));
+    }
+
+
+    //sets target for tracking
+    public void setTarget(@javax.annotation.Nullable LivingEntity pLivingEntity) {
+
+        if (pLivingEntity instanceof Player) {
+            this.setLastHurtByPlayer((Player)pLivingEntity);
+        }
+
+        super.setTarget(pLivingEntity);
+    }
+
+    //required overrides for anger time and target
+    @Override
+    public int getRemainingPersistentAngerTime() {
+        return this.remainingPersistentAngerTime;
+    }
+
+    @Override
+    public void setRemainingPersistentAngerTime(int pTime) {
+        this.remainingPersistentAngerTime = pTime;
+    }
+
+    @Nullable
+    @Override
+    public UUID getPersistentAngerTarget() {
+        return this.persistentAngerTarget;
+    }
+
+    @Override
+    public void setPersistentAngerTarget(@Nullable UUID pTarget) {
+        this.persistentAngerTarget = pTarget;
+    }
+
+    @Override
+    public void startPersistentAngerTimer() {
+        this.setRemainingPersistentAngerTime(PERSISTENT_ANGER_TIME.sample(this.random));
     }
 }
